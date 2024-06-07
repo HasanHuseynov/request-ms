1a9fc68ceca205f5dd712bb9913245d32da4ac97
hasan.huseynov <Bgnnysm200116@>
2024-06-07 00:11:00 -0700

    Adding main project

---
+++ src/test/java/org/government/requestms/RequestMsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.government.requestms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
