12b25d38282d904b6bcebd4aae9059373f5e494d
Javid Arkishiev <javidarkishiev@gmail.com>
2024-06-12 17:44:53 +0400

    Add some changes

---
+++ src/main/java/org/government/requestms/service/JWTService.java
@@ -0,0 +1,66 @@
+package org.government.requestms.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.List;
+import java.util.function.Function;
+
+@Service
+@RequiredArgsConstructor
+public class JWTService {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolvers) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolvers.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignKey() {
+        byte[] key = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(key);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String userName = extractUsername(token);
+        return (userName.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractClaim(token, Claims::getExpiration).before(new Date());
+    }
+
+    public List<String> extractAuthorities(String token) {
+        DecodedJWT decodedJWT = JWT.require(Algorithm.HMAC256(getSignKey().getEncoded()))
+                .build()
+                .verify(token);
+        return decodedJWT.getClaim("authorities").asList(String.class);
+    }
+
+
+}
