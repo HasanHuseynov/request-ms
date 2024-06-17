b07c6ede2a47aacebba2d611e1973ed4a4a75c73
Javid Arkishiev <javidarkishiev@gmail.com>
2024-06-17 09:22:50 +0400

    Add some changes

--- src/main/java/org/government/requestms/config/JwtAuthenticationFilter.java
+++ src/main/java/org/government/requestms/config/JwtAuthenticationFilter.java
@@ -7,13 +7,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.government.requestms.service.JWTService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
