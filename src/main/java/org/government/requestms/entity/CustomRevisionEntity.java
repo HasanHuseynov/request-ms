f85406f126ac026e5f25bbdd2b59439416f572d4
Javid Arkishiev <javidarkishiev@gmail.com>
2024-06-27 01:13:23 +0400

    Add some securtiy function dao ==null

--- src/main/java/org/government/requestms/entity/CustomRevisionEntity.java
+++ src/main/java/org/government/requestms/entity/CustomRevisionEntity.java
@@ -1,20 +1,30 @@
 package org.government.requestms.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.government.requestms.audit.CustomRevisionListener;
 import org.hibernate.envers.DefaultRevisionEntity;
 import org.hibernate.envers.RevisionEntity;
+import org.hibernate.envers.RevisionNumber;
+import org.hibernate.envers.RevisionTimestamp;
 
 @Getter
 @Setter
 @Entity
-@Table(name = "revision_info", schema = "request_ms")
+@Table(name = "rev_info", schema = "request_ms")
 @RevisionEntity(CustomRevisionListener.class)
-public class CustomRevisionEntity extends DefaultRevisionEntity {
+public class CustomRevisionEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @RevisionNumber
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @RevisionTimestamp
+    @Column(name = "timestamp", nullable = false)
+    private long timestamp;
 
     @Column(name = "email")
     private String email;
