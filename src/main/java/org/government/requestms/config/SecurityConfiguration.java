d50ea2a27ebb195df7149012933a8a25937c1ca9
Javid Arkishiev <javidarkishiev@gmail.com>
2024-07-03 22:30:02 +0400

    add new version Exception Handler

--- src/main/java/org/government/requestms/config/SecurityConfiguration.java
+++ src/main/java/org/government/requestms/config/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package org.government.requestms.config;
 
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,7 +17,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
