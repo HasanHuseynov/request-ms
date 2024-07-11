d3dc34864fa30ba5a70a46da7d18feac188fe972
Javid Arkishiev <javidarkishiev@gmail.com>
2024-07-11 21:55:28 +0400

    changed Enum Name

--- src/main/java/org/government/requestms/enums/Status.java
+++ src/main/java/org/government/requestms/enums/Status.java
@@ -1,10 +1,21 @@
 package org.government.requestms.enums;
 
 public enum Status {
-    Gözləmədə,
-    Baxılır,
-    Əssasızdır,
-    Həlledildi,
-    Arxivdədir
+    Gözləmədə("Gözləmədə"),
+    Baxılır("Baxılır"),
+    Əssasızdır("Əssasızdır"),
+    Həlledildi("Həll edildi"),
+    Arxivdədir("Arxivdədir");
+
+    public final String name;
+
+    Status(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
 
 }
