7c688d2732eafd8d58b648bdccc47aad2ee1dd8e
Javid Arkishiev <javidarkishiev@gmail.com>
2024-07-25 00:38:14 +0400

    add InternalServer error in ExceptionHandle. change description and commentText field length

--- src/main/java/org/government/requestms/exception/ExceptionHandle.java
+++ src/main/java/org/government/requestms/exception/ExceptionHandle.java
@@ -5,12 +5,14 @@ import org.government.requestms.dto.response.BaseResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Objects;
 
 @RestControllerAdvice
-public class ExceptionHandle{
+public class ExceptionHandle {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -36,5 +38,11 @@ public class ExceptionHandle{
     @ExceptionHandler(FeignException.NotFound.class)
     public ResponseEntity<?> handleFeignNotFoundException(FeignException.NotFound ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.contentUTF8());
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public BaseResponse<String> handleException(Exception e) {
+        return BaseResponse.fail("Server error : " + e.getMessage());
     }
 }
